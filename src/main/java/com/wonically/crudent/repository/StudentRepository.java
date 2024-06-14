06aa0814e433bcbe07e957d1391f2225fc28bf46
Wony Nguyen <nhd.wyd.2005@gmail.com>
2024-06-14 13:46:08 +0700

    Update Repo and Serv

--- src/main/java/com/wonically/crudent/repository/StudentRepository.java
+++ src/main/java/com/wonically/crudent/repository/StudentRepository.java
@@ -2,6 +2,59 @@ package com.wonically.crudent.repository;
 
 import com.wonically.crudent.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    List<Student> findAll();
+
+    Student findStudentById(Long id);
+
+    Student findStudentByCode(String code);
+
+    List<Student> findStudentByNameContaining(String name);
+
+    List<Student> findStudentByAge(int age);
+
+    List<Student> findStudentByGender(String gender);
+
+    List<Student> findStudentByPhoneNumberContaining(String phoneNumber);
+
+    List<Student> findStudentByEmailContaining(String email);
+
+    List<Student> findStudentByHeightBetweenOrderByHeightAsc(float lowHeight, float highHeight);
+
+    List<Student> findStudentByWeightBetweenOrderByWeightAsc(float lowWeight, float highWeight);
+
+    List<Student> findStudentByBloodTypeStartingWith(String bloodType);
+
+    List<Student> findStudentByNationality(String nationality);
+
+    List<Student> findStudentByCreatedAtAfterOrderByCreatedAtAsc(String createdAt);
+
+    List<Student> findStudentByUpdatedAtAfterOrderByUpdatedAtAsc(String updatedAt);
+
+    void deleteStudentById(Long id);
+
+    void deleteStudentByCode(String code);
+
+    @Modifying
+    @Query("UPDATE Student s SET s.name = :name, s.age = :age, s.gender = :gender, s.phoneNumber = :phoneNumber, s.address = :address, s.email = :email, s.height = :height, s.weight = :weight, s.bloodType = :bloodType, s.nationality = :nationality, s.updatedAt = :updatedAt WHERE s.id = :id")
+    Student updateByStudent(@Param("id") Long id,
+                         @Param("name") String name,
+                         @Param("age") int age,
+                         @Param("gender") String gender,
+                         @Param("phoneNumber") String phoneNumber,
+                         @Param("address") String address,
+                         @Param("email") String email,
+                         @Param("height") float height,
+                         @Param("weight") float weight,
+                         @Param("bloodType") String bloodType,
+                         @Param("nationality") String nationality,
+                         @Param("updatedAt") String updatedAt);
 }
--- src/main/java/com/wonically/crudent/service/StudentService.java
+++ src/main/java/com/wonically/crudent/service/StudentService.java
@@ -1,7 +1,43 @@
 package com.wonically.crudent.service;
 
-import org.springframework.stereotype.Service;
+import com.wonically.crudent.entity.Student;
 
-@Service
+import java.util.List;
+
 public interface StudentService {
+    List<Student> findAll();
+
+    List<Student> findStudentById(Long id);
+
+    List<Student> findStudentByCode(String code);
+
+    List<Student> findStudentByNameContaining(String name);
+
+    List<Student> findStudentByAge(int age);
+
+    List<Student> findStudentByGender(String gender);
+
+    List<Student> findStudentByPhoneNumberContaining(String phoneNumber);
+
+    List<Student> findStudentByEmailContaining(String email);
+
+    List<Student> findStudentByHeightBetweenOrderByHeightAsc(float height);
+
+    List<Student> findStudentByWeightBetweenOrderByWeightAsc(float weight);
+
+    List<Student> findStudentByBloodTypeStartingWith(String bloodType);
+
+    List<Student> findStudentByNationality(String nationality);
+
+    List<Student> findStudentByCreatedAtAfterOrderByCreatedAtAsc(String createdAt);
+
+    List<Student> findStudentByUpdatedAtAfterOrderByUpdatedAtAsc(String updatedAt);
+
+    Student createStudent(Student student);
+
+    Student updateStudent(Long id, Student student);
+
+    void deleteStudentById(Long id);
+
+    void deleteStudentByCode(String code);
 }
